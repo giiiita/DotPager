10c731429309d10571ea5be71c583bb4060c43f9
giiiita <giiiita12t@gmail.com>
2020-03-22 13:15:17 +0900

    inject index and offset

--- Sources/DotPager/DotPager.swift
+++ Sources/DotPager/DotPager.swift
@@ -4,13 +4,9 @@
 //  Created by giiiita on 2020/02/12.
 //  Copyright © 2020 giiiita. All rights reserved.
 //
-
 import SwiftUI
 
 public struct DotPager<Content>: View where Content: View {
-    
-    @State private var index: Int = 0
-    @State private var offset: CGFloat = 0.0
     
     public enum ThresholdType {
         case harf
@@ -32,16 +28,24 @@ public struct DotPager<Content>: View where Content: View {
     private let thresholdType: ThresholdType
     private let normalColor: Color
     private let selectedColor: Color
+    private let pageControlOffsetY: CGFloat
+    @Binding private var index: Int
+    @Binding private var offset: CGFloat
     
     public init(_ data: [Image],
+                index: Binding<Int>,
+                offset: Binding<CGFloat>,
                 contentWidth: CGFloat,
                 contentHeight: CGFloat,
                 contentMode: ContentMode = .fill,
                 thresholdType: ThresholdType = .harf,
                 normalColor: Color = Color.gray,
                 selectedColor: Color = Color.black,
+                pageControlOffsetY: CGFloat = 20.0,
                 content: @escaping (Image) -> Content) {
         self.data = data.map { $0 }
+        self._index = index
+        self._offset = offset
         self.content = content
         self.contentWidth = contentWidth
         self.contentHeight = contentHeight
@@ -49,47 +53,50 @@ public struct DotPager<Content>: View where Content: View {
         self.thresholdType = thresholdType
         self.normalColor = normalColor
         self.selectedColor = selectedColor
+        self.pageControlOffsetY = pageControlOffsetY
     }
     
     public var body : some View {
-        GeometryReader { geometry in
-            VStack(alignment: .center) {
+            ZStack(alignment: .center) {
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 0) {
                         ForEach(0..<self.data.count) {
                             self.content(self.data[$0].resizable())
-                                .frame(width: self.contentWidth, height: self.contentHeight)
-                                .aspectRatio(contentMode: self.contentMode)
+                                .frame(width: self.contentWidth)
+                                .aspectRatio(self.contentWidth / self.contentHeight, contentMode: self.contentMode)
                         }
                     }
                 }
                 .content.offset(x: self.offset)
-                .frame(width: geometry.size.width, height: self.contentHeight, alignment: .leading)
+                .frame(width: self.contentWidth, height: self.contentHeight, alignment: .leading)
                 .gesture(DragGesture()
                     .onChanged({ value in
                           if self.index == self.data.endIndex - 1 && value.translation.width < 0 { return }
                           if self.index == 0 && value.translation.width > 0 { return }
-                          self.offset = value.translation.width - geometry.size.width * CGFloat(self.index)
+                          self.offset = value.translation.width - self.contentWidth * CGFloat(self.index)
                       })
                     .onEnded({ value in
-                        if value.predictedEndTranslation.width < -self.thresholdType.getSize(value: geometry.size.width) {
+                        if value.predictedEndTranslation.width < -self.thresholdType.getSize(value: self.contentWidth) {
                             self.index = min(self.index + 1, self.data.endIndex - 1)
-                        } else if value.predictedEndTranslation.width > self.thresholdType.getSize(value: geometry.size.width) {
+                        } else if value.predictedEndTranslation.width > self.thresholdType.getSize(value: self.contentWidth) {
                             self.index = max(self.index - 1, 0)
                         }
                         
                         withAnimation {
-                            self.offset = -geometry.size.width * CGFloat(self.index)
+                            self.offset = -self.contentWidth * CGFloat(self.index)
                         }
                     })
                 )
-                PageControlView(
-                    elementCount: self.data.count,
-                    normalColor: self.normalColor,
-                    selectedColor: self.selectedColor,
-                    index: self.$index
-                )
-            }
+                
+                VStack(alignment: .center, spacing: 0) {
+                    Spacer()
+                    PageControlView(
+                         elementCount: self.data.count,
+                         normalColor: self.normalColor,
+                         selectedColor: self.selectedColor,
+                         index: self.$index
+                    ).offset(x: 0, y: self.pageControlOffsetY)
+                }
         }
     }
 }
@@ -102,11 +109,11 @@ private struct PageControlView: View {
     @Binding var index: Int
     
     var body: some View {
-        HStack(alignment: .center, spacing: 2.0) {
+        HStack(alignment: .center, spacing: 8.0) {
             ForEach(0..<self.elementCount) { index in
                 Circle().frame(width: 10, height: 10)
                     .foregroundColor((self.index == index) ?  self.selectedColor : self.normalColor)
             }
-        }
+        }.padding(.bottom, 130)
     }
 }
